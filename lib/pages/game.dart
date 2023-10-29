5d9e4f334bc1d0e41f73bdbbc98c2ec07b0fb16f
suryansh omar <suryanshomar@Suryansh.local>
2023-10-29 17:02:18 +0530

    network assets reliablity

--- lib/pages/game.dart
+++ lib/pages/game.dart
@@ -1,16 +1,7 @@
-import 'dart:math' as math;
-import 'package:flame/collisions.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flame/components.dart';
-import 'package:flame/events.dart';
 import 'package:flame/game.dart';
-import 'package:flame/palette.dart';
-import 'package:flame/sprite.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:yc_game_poc/components/obstacle.dart';
 import 'package:yc_game_poc/components/player.dart';
-import 'package:yc_game_poc/providers/providers.dart';
 
 class TryGame extends FlameGame with HasCollisionDetection {
   final WidgetRef ref;
@@ -19,8 +10,8 @@ class TryGame extends FlameGame with HasCollisionDetection {
 
   @override
   Future<void> onLoad() async {
-    add(Player(ref: ref));
     add(Obstacle(ref: ref));
+    add(Player(ref: ref));
     // camera.follow(man);
   }
 
